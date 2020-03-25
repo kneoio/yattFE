f4f8c379a1b6a4e6d58cc895c1fe9c2ed77b8377
justaidajam@gmail.com <justaidajam@gmail.com>
2020-03-26 00:20:27 +0200

    #

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
+import Navigator from './components/Navigator.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Navigator/>
     </div>
   );
 }
 
 export default App;
+
+
+
+
